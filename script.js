1f29724b7faed41d21a6cb3109d4e2e593772192
Juan L C <202941136+jlcarg@users.noreply.github.com>
2025-04-27 12:15:34 +0900

    Write function to create the board feature

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const container = document.querySelector(".container");
-const newPara = document.createElement("p");
-newPara.textContent = "Hello";
-container.appendChild(newPara);
+function drawBoard(numberOfSquares) {
+    const drawingBoard = document.querySelector(".drawing-board");
+    const BOARDWIDTH = 960;
+    let squareDimensions = BOARDWIDTH / numberOfSquares;
+    console.log(typeof squareDimensions);
+    console.log(squareDimensions);
+
+    for (i = 0; i < numberOfSquares * numberOfSquares; i++) {
+        const newSquare = document.createElement("div");
+        newSquare.setAttribute("class", "square");
+        newSquare.style.width = squareDimensions + 'px';
+        newSquare.style.height = squareDimensions + 'px';
+        drawingBoard.appendChild(newSquare);
+    }
+
+    return console.log(`A drawing board of ${numberOfSquares} x ${numberOfSquares} squares has been created.`)
+}
+
+drawBoard(16);
